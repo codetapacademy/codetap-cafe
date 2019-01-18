621286a9751993aea7136a327d8d2b6acc14c8fb
Marian Zburlea <marianzburlea@gmail.com>
2019-01-18 11:18:34 +0000

    render list of names with firebase firestore

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import { firestore } from "./firebase";
+
 class App extends Component {
+  state = {
+    nameList: []
+  };
+  componentDidMount() {
+    firestore
+      .collection("chat")
+      .get()
+      .then(snapshot => {
+        this.setState({
+          nameList: snapshot.docs.map(doc => doc.data())
+        });
+      });
+  }
+  renderNameList = () => {
+    const { nameList } = this.state;
+    return nameList.map(({ name }) => {
+      const id = `name-id-${Math.random()}`;
+      return <div key={id}>{name}</div>;
+    });
+  };
   render() {
-    return <div className="App">Neata</div>;
+    return (
+      <div className="App">
+        List of names
+        {this.renderNameList()}
+      </div>
+    );
   }
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const config = {
+  apiKey: "AIzaSyCr8ZedBYEGq-WyB8XSMHWRcX9mdJq3on0",
+  authDomain: "codetap-cafe.firebaseapp.com",
+  databaseURL: "https://codetap-cafe.firebaseio.com",
+  projectId: "codetap-cafe",
+  storageBucket: "codetap-cafe.appspot.com",
+  messagingSenderId: "878157907725"
+};
+
+firebase.initializeApp(config);
+const firestore = firebase.firestore();
+
+export { firestore };
