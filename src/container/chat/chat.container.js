79ca9bd36f4eb2d8f607da79c95d94e19289ce11
Marian Zburlea <marianzburlea@gmail.com>
2019-01-25 21:48:38 +0000

    refactor the style of chat into chat.style.js

--- src/container/chat/chat.container.js
+++ src/container/chat/chat.container.js
@@ -1,47 +1,15 @@
 import React, { useState, useEffect, useContext } from "react";
-import styled from "styled-components";
 import TextArea from "../../component/text-area";
 import { firebase, firestore } from "../../firebase";
 import { getState, DispatchContext } from "../../redux";
 import Button from "../../component/button";
-
-const ChatWrapper = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 200px;
-  grid-template-rows: 1fr 60px;
-  height: 100%;
-  grid-gap: 5px;
-  padding: 5px;
-`;
-
-const ChatBody = styled.div`
-  grid-column: 1/2;
-  grid-row: 1/2;
-  display: grid;
-  grid-template-columns: auto 1fr;
-  grid-gap: 10px;
-`;
-
-const ChatMessage = styled.div`
-  display: grid;
-  grid-gap: 10px;
-  grid-template-columns: 120px 1fr;
-`;
-
-const ChatUser = styled.div`
-  font-weight: bold;
-  white-space: nowrap;
-`;
-
-const TextAreaWrapper = styled.div`
-  grid-column: 1/2;
-  grid-row: 2/3;
-`;
-
-const ButtonWrapper = styled.div`
-  grid-column: 2/3;
-  grid-row: 2/3;
-`;
+import {
+  ChatWrapper,
+  ChatBody,
+  ChatUser,
+  TextAreaWrapper,
+  ButtonWrapper
+} from "./chat.style";
 
 const Chat = () => {
   const [currentMessage, setCurrentMessage] = useState("");
