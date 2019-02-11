9f6c35efbb22d2cfd24f65b7d8683e3141e06c76
Marian Zburlea <marianzburlea@gmail.com>
2019-02-11 10:16:11 +0000

    Basic membership functionality working

--- src/container/user-list/user-list.component.js
+++ src/container/user-list/user-list.component.js
@@ -4,16 +4,28 @@ import { getState } from "../../redux";
 import { DispatchContext } from "../../redux";
 import { UPDATE_USER_LIST } from "./constant";
 import Button from "../../component/button";
+import { UserItemWrapper } from "./user-list.style";
 
-const UserList = ({}) => {
+const UserList = () => {
   const urerRef = firestore.collection("user");
+  const memberRef = firestore.collection("member");
   const dispatch = useContext(DispatchContext);
   const { data: userList } = getState("userList");
   const { data: memberList } = getState("memberList");
   console.log(userList, memberList);
 
   const toggleMember = uid => {
-    console.log(`Togge member`);
+    console.log(`Togge member ${uid}`);
+    const isMember = memberList.filter(member => member.id === uid);
+    if (isMember.length) {
+      memberRef.doc(uid).delete();
+    } else {
+      memberRef
+        .doc(uid)
+        .set(userList.filter(user => user.uid === uid).reduce(u => u), {
+          merge: true
+        });
+    }
   };
 
   const isMemberLabel = uid => {
@@ -24,10 +36,15 @@ const UserList = ({}) => {
 
   const renderUserList = () => {
     return userList.map(({ displayName, uid }) => (
-      <Fragment key={uid}>
+      <UserItemWrapper key={uid}>
         <div>{displayName}</div>
-        <Button label={isMemberLabel(uid)} />
-      </Fragment>
+        <div>
+          <Button
+            label={isMemberLabel(uid)}
+            onClick={() => toggleMember(uid)}
+          />
+        </div>
+      </UserItemWrapper>
     ));
   };
 
