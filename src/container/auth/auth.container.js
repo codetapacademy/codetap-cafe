6c9d900ce6914d2a642e4551d8727bfa4c9bf1b5
Marian Zburlea <marianzburlea@gmail.com>
2019-02-12 10:11:00 +0000

    move online status from member table to user table

--- src/container/auth/auth.container.js
+++ src/container/auth/auth.container.js
@@ -18,7 +18,6 @@ const Auth = () => {
 
   const onlineRef = database.ref(".info/connected");
   const userRef = firestore.collection("user");
-  const memberRef = firestore.collection("member");
 
   useEffect(() => {
     // check if the user is already logged in
@@ -40,12 +39,12 @@ const Auth = () => {
               database.ref(`/status/${uid}`).set("online");
 
               // Check if the user exists in the member firestore and update its status if found
-              memberRef
+              userRef
                 .doc(uid)
                 .get()
                 .then(snap => {
                   if (snap.exists) {
-                    memberRef.doc(uid).set(
+                    userRef.doc(uid).set(
                       {
                         status: "online"
                       },
@@ -105,6 +104,19 @@ const Auth = () => {
         dispatch(updateUser(null));
       })
       .catch(error => console.log(`Sign out failed!`, error));
+    // userRef
+    //   .doc(uid)
+    //   .get()
+    //   .then(snap => {
+    //     if (snap.exists) {
+    //       userRef.doc(uid).set(
+    //         {
+    //           status: "offline"
+    //         },
+    //         { merge: true }
+    //       );
+    //     }
+    //   });
   };
 
   const loginButtonProperties = {
